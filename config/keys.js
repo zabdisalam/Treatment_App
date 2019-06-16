3d92dee4720ada81bcc3828b4992e8e84500d574
zabdisalam <kofigarebulasp@gmail.com>
2019-06-16 16:24:55 -0400

    first commit from lenovo

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://root:4900123@cluster0-qsmeu.mongodb.net/test?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://root:4900123@cluster0-qsmeu.mongodb.net/Admin_Portal?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
